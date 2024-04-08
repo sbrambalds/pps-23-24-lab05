7b0893b420c50dde9a9400017cb51ca5d63fbd4c
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-04-08 12:11:37 +0200

    Create add method for type sequence

--- src/main/scala/util/Sequences.scala
+++ src/main/scala/util/Sequences.scala
@@ -51,6 +51,9 @@ object Sequences: // Essentially, generic linkedlists
       def reverse(): Sequence[A] = sequence match
         case Cons(h, t) => t.reverse().concat(Cons(h, Nil()))
         case _ => Nil()
+
+      def add(a: A): Sequence[A] = sequence.concat(Cons(a, Nil()))
+
 @main def trySequences =
   import Sequences.* 
   val sequence = Sequence(1, 2, 3)
