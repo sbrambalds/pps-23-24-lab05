0700ec1675bd09a28bf828acad7825d4e216e8f8
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-04-09 08:50:36 +0200

    Create Warehouse and item classes

--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -1,14 +1,17 @@
 package ex
 
 import util.Optionals.Optional
-import util.Sequences.*
+import util.Sequences.Sequence
 trait Item:
   def code: Int
   def name: String
   def tags: Sequence[String]
 
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ItemImpl(code, name, tags)
+
+  private case class ItemImpl(override val code: Int, override val name: String, override val tags: Sequence[String]) extends Item
+
 
 /**
  * A warehouse is a place where items are stored.
@@ -45,7 +48,51 @@ trait Warehouse:
 end Warehouse
 
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(): Warehouse = WarehouseImpl(Sequence.empty)
+
+  private case class WarehouseImpl(private var items: Sequence[Item]) extends Warehouse:
+    /**
+     * Stores an item in the warehouse.
+     *
+     * @param item the item to store
+     */
+    override def store(item: Item): Unit =
+      items = items.add(item)
+
+    /**
+     * Searches for items with the given tag.
+     *
+     * @param tag the tag to search for
+     * @return the list of items with the given tag
+     */
+    override def searchItems(tag: String): Sequence[Item] =
+      items.filter(v => v.tags.contains(tag))
+
+    /**
+     * Retrieves an item from the warehouse.
+     *
+     * @param code the code of the item to retrieve
+     * @return the item with the given code, if present
+     */
+    override def retrieve(code: Int): Optional[Item] =
+      items.find(v => v.code == code)
+
+    /**
+     * Removes an item from the warehouse.
+     *
+     * @param item the item to remove
+     */
+    override def remove(item: Item): Unit =
+      items = items.filter(v => v != item)
+
+    /**
+     * Checks if the warehouse contains an item with the given code.
+     *
+     * @param itemCode the code of the item to check
+     * @return true if the warehouse contains an item with the given code, false otherwise
+     */
+    override def contains(itemCode: Int): Boolean = items.filter(v => v.code == itemCode) != Sequence.empty
+
 
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
@@ -54,17 +101,17 @@ object Warehouse:
   val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
   val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
 
-  warehouse.contains(dellXps.code) // false
+  println(warehouse.contains(dellXps.code)) // false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
-  warehouse.contains(dellXps.code) // true
+  println(warehouse.contains(dellXps.code)) // true
   warehouse.store(dellInspiron) // side effect, add dell Inspiron to the warehouse
   warehouse.store(xiaomiMoped) // side effect, add xiaomi moped to the warehouse
-  warehouse.searchItems("mobility") // Sequence(xiaomiMoped)
-  warehouse.searchItems("notebook") // Sequence(dellXps, dell Inspiron)
-  warehouse.retrieve(11) // None
-  warehouse.retrieve(dellXps.code) // Just(dellXps)
+  println(warehouse.searchItems("mobility")) // Sequence(xiaomiMoped)
+  println(warehouse.searchItems("notebook")) // Sequence(dellXps, dell Inspiron)
+  println(warehouse.retrieve(11)) // None
+  println(warehouse.retrieve(dellXps.code)) // Just(dellXps)
   warehouse.remove(dellXps) // side effect, remove dell xps from the warehouse
-  warehouse.retrieve(dellXps.code) // None
+  println(warehouse.retrieve(dellXps.code)) // None
 
 /** Hints:
  * - Implement the Item with a simple case class
